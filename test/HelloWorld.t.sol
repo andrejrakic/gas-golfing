ddb8a92c1f1076abaef9c276cfbdd053daee9e92
andrejrakic <andrej.rakic@mvpworkshop.co>
2022-07-05 12:02:07 +0200

    Forge init

---
+++ test/HelloWorld.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import 'forge-std/Test.sol';
+import 'src/HelloWorld.sol';
+
+contract HelloWorldTest is Test {
+	HelloWorld helloWorld;
+
+	function setUp() public {
+		helloWorld = new HelloWorld('Foundry is fast!');
+	}
+
+	function testShouldBeDeployed() public {
+		assertEq(helloWorld.greet(), 'Foundry is fast!');
+	}
+
+	function testShouldUpdateGreeting() public {
+		assertEq(helloWorld.version(), 0);
+		helloWorld.updateGreeting('Hello, World!');
+
+		assertEq(helloWorld.version(), 1);
+		assertEq(helloWorld.greet(), 'Hello, World!');
+
+		emit log(helloWorld.greet()); // forge test -vv
+	}
+
+	function testShouldFuzz(string memory _greeting) public {
+		helloWorld.updateGreeting(_greeting);
+		assertEq(helloWorld.greet(), _greeting);
+	}
+}
